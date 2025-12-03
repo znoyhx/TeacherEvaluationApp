5ba324e1af569fe5a0f3f64b0a20f2a80e6a3491
PlutoRedbean <HD3197388203@outlook.com>
2025-12-03 10:41:16 +0800

    comment: interface readme comments

--- src/main/java/com/stu/mapper/EvaluationMapper.java
+++ src/main/java/com/stu/mapper/EvaluationMapper.java
@@ -7,11 +7,15 @@ import java.util.List;
 
 @Mapper
 public interface EvaluationMapper {
+    // 根据学生ID和教师ID获取评价
     Evaluation getByStudentIdAndTeacherId(Long studentId, Long teacherId);
 
+    // 插入新的评价(TODO: 内部没有对数据库内是否已有该评价进行检查)
     void insert(Evaluation evaluation);
 
+    // 根据教师ID获取所有评价
     List<Evaluation> getByTeacherId(Long teacherId);
 
+    // 根据用户ID获取所有评价
     List<Evaluation> getByUserId(Long currentId);
 }
--- src/main/java/com/stu/mapper/TeacherMapper.java
+++ src/main/java/com/stu/mapper/TeacherMapper.java
@@ -5,12 +5,15 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TeacherMapper {
-
+    // 插入新的老师数据(TODO: 内部没有对数据库内是否已有该评价进行检查)
     void insert(Teacher teacher);
 
+    // 更新老师数据
     void update(Teacher teacher);
 
+    // 根据教师ID获取教师信息
     Teacher getByTeacherId(Long id);
 
+    // 根据教师姓名获取教师信息
     Teacher getByName(String name);
 }
