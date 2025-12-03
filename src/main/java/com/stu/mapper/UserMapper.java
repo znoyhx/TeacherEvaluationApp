fb3b56512bed3989cc685e7fe94ab5a4d881a978
PlutoRedbean <HD3197388203@outlook.com>
2025-12-03 10:37:19 +0800

    remove: ibatis impl

--- src/main/java/com/stu/mapper/UserMapper.java
+++ src/main/java/com/stu/mapper/UserMapper.java
@@ -12,16 +12,12 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    @Select("select * from userdata where username = #{username}")
     User getByUsername(String username);
 
-    @Insert("insert into userdata (username, password, nickname, phone_number) values (#{username}, #{password}, #{nickname}, #{phoneNumber})")
     void insert(User user);
 
-    @Select("select * from userdata where nickname like CONCAT('%', #{nickname}, '%')")
     List<User> getByNickname(String nickname);
 
-    @Select("select * from userdata where id = #{id}")
     User getById(Long id);
 
 
